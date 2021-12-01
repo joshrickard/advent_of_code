266f6740d04420027f277b6ff0e19fdfe5b6cd70
Josh Rickard <josh.rickard@gmail.com>
2021-12-01 13:09:28 -0600

    Update spec template

--- spec/2021/day_01_spec.cr
+++ spec/2021/day_01_spec.cr
@@ -3,40 +3,36 @@ require "../../src/2021/day_01"
 
 describe Year2021::Day01 do
   it "solves part 1" do
-    input = IO::Memory.new(
-      <<-EOS
-        199
-        200
-        208
-        210
-        200
-        207
-        240
-        269
-        260
-        263
-      EOS
-    )
-    result = Year2021::Day01::Part1.run(input)
+    example = <<-EOS.split.map(&.strip).join("\n")
+      199
+      200
+      208
+      210
+      200
+      207
+      240
+      269
+      260
+      263
+    EOS
+    result = Year2021::Day01::Part1.run(IO::Memory.new(example))
     result.should eq(7)
   end
 
   it "solves part 2" do
-    input = IO::Memory.new(
-      <<-EOS
-        199
-        200
-        208
-        210
-        200
-        207
-        240
-        269
-        260
-        263
-      EOS
-    )
-    result = Year2021::Day01::Part2.run(input)
+    example = <<-EOS.split.map(&.strip).join("\n")
+      199
+      200
+      208
+      210
+      200
+      207
+      240
+      269
+      260
+      263
+    EOS
+    result = Year2021::Day01::Part2.run(IO::Memory.new(example))
     result.should eq(5)
   end
 end

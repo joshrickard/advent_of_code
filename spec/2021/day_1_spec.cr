a0376100e0cb9dc25eb2dfd87be841dae0c189af
Josh Rickard <josh.rickard@gmail.com>
2021-12-02 13:25:12 -0600

    Fix naming & indentation

--- spec/2021/day_1_spec.cr
+++ spec/2021/day_1_spec.cr
@@ -3,34 +3,34 @@ require "../../src/2021/day_1"
 
 describe Year2021::Day1 do
   it "solves part 1" do
-    example = <<-EOS.split.map(&.strip).join("\n")
-      199
-      200
-      208
-      210
-      200
-      207
-      240
-      269
-      260
-      263
+    example = <<-EOS
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
     EOS
     result = Year2021::Day1::Part1.run(IO::Memory.new(example))
     result.should eq(7)
   end
 
   it "solves part 2" do
-    example = <<-EOS.split.map(&.strip).join("\n")
-      199
-      200
-      208
-      210
-      200
-      207
-      240
-      269
-      260
-      263
+    example = <<-EOS
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
     EOS
     result = Year2021::Day1::Part2.run(IO::Memory.new(example))
     result.should eq(5)
